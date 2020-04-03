a0700ff5965f6f6614ab0050cb517431df983265
Carlos Gonzalez <carlos.rgn@gmail.com>
2020-04-02 23:16:14 -0600

    formatting

--- apps/ReporterCli/Program.cs
+++ apps/ReporterCli/Program.cs
@@ -7,15 +7,15 @@ using Dapper;
 using Npgsql;
 
 namespace ReporterCli {
-    class Program {
-        public static async Task Main(string[] args) {
-            await using(var employeeRepository = new EmployeeRepository()) {
-                var employees = await employeeRepository.GetAll();
-                Console.WriteLine($"Employee count: {employees.AsList().Count}");
-                foreach (var employee in employees) {
-                    Console.WriteLine($"Name: {employee.Name}");
-                }
-            }
+  class Program {
+    public static async Task Main(string[] args) {
+      await using(var employeeRepository = new EmployeeRepository()) {
+        var employees = await employeeRepository.GetAll();
+        Console.WriteLine($"Employee count: {employees.AsList().Count}");
+        foreach (var employee in employees) {
+          Console.WriteLine($"Name: {employee.Name}");
         }
+      }
     }
+  }
 }
